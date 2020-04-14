0a1a196ff8faf27ede294745bfeb4028cfe388b9
Bryan Naegele <bnaegele@weedmaps.com>
2020-04-14 16:52:13 -0600

    package prep

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule SocketDrano.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :socket_drano,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
+      preferred_cli_env: preferred_cli_env(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -18,12 +24,42 @@ defmodule SocketDrano.MixProject do
     ]
   end
 
+  defp docs do
+    [
+
+      canonical: "http://hexdocs.pm/socket_drano",
+      source_url: "https://github.com/bryannaegele/socket_drano",
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp preferred_cli_env do
+    [
+      docs: :docs
+    ]
+  end
+
+  defp description do
+    """
+    Phoenix Socket and HTTP drainer.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Bryan Naegele"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/bryannaegele/socket_drano"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:phoenix, ">= 1.4.7", optional: true},
       {:ranch, ">= 1.7.0", optional: true},
-      {:telemetry, "~> 0.4", optional: true}
+      {:telemetry, "~> 0.4", optional: true},
+      {:ex_doc, "~> 0.21", only: [:dev, :docs]}
     ]
   end
 end
